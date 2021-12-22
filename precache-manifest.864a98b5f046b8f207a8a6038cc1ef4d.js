990c5a4422e0854b91d85479b2c405f59de9180f
Mstars_Lab1_13 <mstars_lab1_13@Mstars-Lab1-13s-iMac.local>
2021-12-22 11:44:08 +0800

    Updates

---
+++ precache-manifest.864a98b5f046b8f207a8a6038cc1ef4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4bea573279516cf486e",
+    "url": "/Course-directory/static/css/main.434622c6.chunk.css"
+  },
+  {
+    "revision": "f4bea573279516cf486e",
+    "url": "/Course-directory/static/js/main.f4bea573.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/Course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "902367bbcba9f658bdc8",
+    "url": "/Course-directory/static/js/runtime~main.902367bb.js"
+  },
+  {
+    "revision": "48821d870d2011af59a8b2f786a59797",
+    "url": "/Course-directory/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Course-directory/precache-manifest.864a98b5f046b8f207a8a6038cc1ef4d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Course-directory/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
